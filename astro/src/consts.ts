8b5c678bc380788f4cb40ba0325c8eca0ab430bc
Plamen Minev <pacominev@gmail.com>
2024-11-17 18:14:00 +0200

    fix: website title

--- astro/src/consts.ts
+++ astro/src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Paco Minev\'s Blog';
+export const SITE_DESCRIPTION = 'Welcome to my blog!';
 export const LINKS = {
     github: "https://github.com/pminev",
     twitter: "https://x.com/MinevPlamen",
